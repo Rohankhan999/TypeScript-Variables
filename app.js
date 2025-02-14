891d83b81678bca63ba11568f5a6a81aee49c09a
Rohankhan999 <rohankhan08453@gmail.com>
2025-02-14 16:20:11 +0500

    String Done

---
+++ app.js
@@ -0,0 +1,8 @@
+// Variables for string
+var FruitsName = "Apple";
+// console.log(FruitsName);
+var AnimalName = "Bear";
+// console.log(AnimalName);
+var VegetableName = "Pumpkin";
+// console.log(VegetableName);
+var BrandName = "Toyota";
---
+++ app.ts
@@ -0,0 +1,9 @@
+// Variables for type  string
+
+var FruitsName: String = "Apple";
+var AnimalName: String = "Bear";
+var VegetableName: String = "Pumpkin";
+var BrandName: String = "Toyota";
+var DrinkName: string = "RedBull";
+
+// Variables for type Number
